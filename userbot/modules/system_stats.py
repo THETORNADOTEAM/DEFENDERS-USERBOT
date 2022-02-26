836db1fca89d24dfa75b93b4ef6809471af28d97
SIDDHARTH SRIDHAR <84910325+THETORNADOTEAM@users.noreply.github.com>
2022-02-26 21:30:23 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -93,7 +93,7 @@ async def amireallyalive(alive):
         f"⚡️𝗠𝗬 𝗢𝗪𝗡𝗘𝗥⚡️➪{DEFAULTUSER}** \n" 
         f"**⚡️𝗥𝗘𝗣𝗢⚡️➪ [DefendersUb](https://github.com/THETORNADOTEAM/KAAL-BOT)** \n" 
         f"**⚡️𝗖𝗛𝗔𝗡𝗡𝗘𝗟⚡️➪ [𝗝𝗢𝗜𝗡](https://t.me/ESPECTRAL_REVENGE_USERBOT)** \n" 
-        f"**⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️➪ [𝗝𝗢𝗜𝗡](https://t.me/DEFENDERS_UB_UPDATES)**")
+        f"**⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️➪ [𝗝𝗢𝗜𝗡](https://t.me/DEFENDERS_UB_UPDATES)** \n"
     )
     if ALIVE_LOGO:
         try:
